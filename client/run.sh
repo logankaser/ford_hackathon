a012e725a0dc51785dc2c5daf393523b06824aba
Logan James Kaser <logankaser@fyrn.io>
2018-08-14 17:17:50 -0700

    Add database support

--- client/run.sh
+++ client/run.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+if ! [ -e venv ]
+then
+virtualenv venv
+pip3 install -r requirements.txt
+fi
 . venv/bin/activate
-FLASK_APP="vehicle_client" flask run
+
+FLASK_APP="vehicle_client" FLASK_DEBUG="TRUE" flask run
--- server/run.sh
+++ server/run.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+if ! [ -e venv ]
+then
+virtualenv venv
+pip3 install -r requirements.txt
+fi
 . venv/bin/activate
-FLASK_APP="app_server" flask run
+
+FLASK_APP="app_server" FLASK_DEBUG="TRUE" flask run
