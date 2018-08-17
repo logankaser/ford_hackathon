467609873a66775f37b5b1a1ca06f974eaf8f978
Logan James Kaser <logankaser@fyrn.io>
2018-08-16 18:22:46 -0700

    better auth

--- server/app_server/admin.py
+++ server/app_server/admin.py
@@ -4,10 +4,12 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from app_server import db
+from app_server.auth import admin_required
 
 bp = Blueprint("admin", __name__, url_prefix="/admin")
 
 
 @bp.route("/")
+@admin_required
 def index():
     return "Admin test"
