6c2d7b890e377df807aad1173d8b7cff494b64f8
zedin27 <ztisneshamzeh@csumb.edu>
2018-08-20 17:30:47 -0700

    documented the modeles file

--- sphinx/source/code-test.rst
+++ sphinx/source/code-test.rst
@@ -1,10 +1,20 @@
 Documentation for the Code
 **************************
 
-#1 models
+Models
 =========================
 
-Description:
-
 .. automodule:: models
+   :members: hash_password
+
+As you see, we will need to use our function **hash_password** to encode and secure passwords
+
+User class
+----------
+.. autoclass:: User
+   :members: __init__, get_token, check_token
+
+AppEntry class
+--------------
+.. autoclass:: AppEntry
    :members:
--- sphinx/source/models.py
+++ sphinx/source/models.py
@@ -20,40 +20,104 @@ Global variables::
 
 """
 
-
-def public_fn_with_googley_docstring(name, state=None):
-    """This function does something. This is an example, no worries
-
-    Args:
-       name (str):  The name to use.
-
-    Kwargs:
-       state (bool): Current state to be in.
-
-    Returns:
-       int.  The return code::
-
-          0 -- Success!
-          1 -- No good.
-          2 -- Try again.
-
-    Raises:
-       AttributeError, KeyError
-
-    A really great idea.  A way you might use me is
-
-    >>> print public_fn_with_googley_docstring(name='foo', state=None)
-    0
-
-    BTW, this always returns 0.  **NEVER** use with :class:`MyPublicClass`.
-
-    """
-    return 0
-
 def hash_password(plain_text):
 	"""
-	This is a test #9000
-	returns decoded hash (I need an example here)::
-		(test here)
+	Hashes the given string from the parameter
+
+	:param plain_text: something something
+	:type plain_text: str.
+
+	:returns: hashed.decode()
+
+	As you see, we will need to use our :func:: hash_password to encode and secure passwords
+	
+	"""
+
+class User(object):
+	"""
+	This is a public class with the user database's methods::
+
+		__init__(self, email, username, password, dev=False, admin=False)
+		get_token(self, user_id)
+		check_token(auth_token)
+
+	"""
+
+	def __init__(self, email, username, password, dev=False, admin=False):
+		"""
+		Create a new registered user
+
+		:param email: user's email
+		:type email: str.
+
+		:param username: user's login name
+		:type username: str.
+
+		:param password: user's password
+		:type password: str.
+
+		:param dev: flag state if user is developer
+		:type dev: bool.
+
+		:param admin: flag state if user is admin
+		:type admin: bool.
+
+		Class details::
+
+			self.username = username
+			self.email = email
+			self.password_hash = hash_password(password)
+			self.created = datetime.now()
+			self.admin = admin
+			self.dev = dev
+
+		"""
+
+	def get_token(self, user_id):
+		"""
+		Generates an API auth token
+
+		:returns: string -- the return code
+
+		"""
+		try:
+			payload = {
+				"exp": datetime.utcnow() + timedelta(days=1),
+				"iat": datetime.utcnow(),
+				"sub": self.id
+			}
+			return jwt.encode(
+				payload,
+				app.config.get("SECRET_KEY"),
+				algorithm="HS256"
+			)
+		except Exception as e:
+			return e
+
+	@staticmethod
+	def check_token(auth_token):
+		"""
+
+		Decodes the auth token
+
+		:param auth_token: secret key
+		:type auth_token: str.
+
+		:returns: User|none
+
+		"""
+
+class AppEntry(object):
+	"""
+	Class with the app details::
+
+		__tablename__ = "app_entry"
+		id = db.Column(db.Integer, primary_key=True)
+		name = db.Column(db.String(50), nullable=False)
+		description = db.Column(db.String(1000), nullable=False)
+		created = db.Column(db.DateTime, nullable=False)
+		updated = db.Column(db.DateTime, nullable=False)
+		downloads = db.Column(db.Integer, nullable=False)
+		dev_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
 	"""
