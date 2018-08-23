398e361d41f18ae1e8973763196a51d65c741aee
theo-walton <theodore_walton@hotmail.com>
2018-08-23 14:20:18 -0700

    added admin page and special app/user pages that admin can see

--- server/app_server/admin.py
+++ server/app_server/admin.py
@@ -7,15 +7,47 @@ from flask import (
 )
 from app_server import db
 from app_server.auth import admin_required
+from app_server.models import AppEntry, User
 
 bp = Blueprint("admin", __name__, url_prefix="/admin")
 
 
-@bp.route("/")
+@bp.route("/", methods=["GET"])
 @admin_required
-def index():
-    """Admin test.
+def admin_home():
+    apps = AppEntry.query.filter_by(approved=False).limit(20)
+    return render_template("admin_profile.html", apps=apps)
 
-    :returns: admin test
-    """
-    return "Admin test"
+
+@bp.route("/user/search/<keyword>", methods=["GET", "POST"])
+@admin_required
+def admin_user_search(keyword):
+    users = User.query.msearch(keyword, fields=["id", "username", "email"]).\
+        limit(100)
+    return render_template("admin_user_search.html", users=users)
+
+
+@bp.route("/app/search/<keyword>", methods=["GET", "POST"])
+@admin_required
+def admin_app_search(keyword):
+    apps = AppEntry.query.msearch(keyword, fields=["id", "name", "description"]).\
+        limit(100)
+    return render_template("admin_app_search.html", apps=apps)
+
+
+@bp.route("/app/<app_id>", methods=["GET"])
+@admin_required
+def admin_app_view(app_id):
+    app = AppEntry.query.get(app_id)
+    if not app:
+        return ("App not found", 400)
+    user = User.query.get(app.dev_id)
+    return render_template("admin_app_view.html", app=app, devname=user.username)
+
+@bp.route("/user/<user_id>", methods=["GET"])
+@admin_required
+def admmin_app_view(user_id):
+    user = User.query.get(user_id)
+    if not user:
+        return ("User not found", 400)
+    return render_template("admin_user_view.html", user=user)
