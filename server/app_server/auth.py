36dda13e9031f93be435b9b1a692ca290685c492
theo-walton <theodore_walton@hotmail.com>
2018-08-26 20:02:52 -0700

    added email notifications to this branch because reasons

--- server/app_server/auth.py
+++ server/app_server/auth.py
@@ -11,7 +11,9 @@ from flask import (
 )
 from app_server import db, bcrypt
 from app_server.models import User, hash_password
-from app_server.forms import RegisterForm, LoginForm, ChangePasswordForm, ResetPasswordForm
+from app_server.forms import (
+    RegisterForm, LoginForm, ChangePasswordForm, ResetPasswordForm
+)
 from secrets import randbelow
 
 bp = Blueprint("auth", __name__)
@@ -194,8 +196,10 @@ def change_password():
     if form.validate_on_submit():
         if request.form["newPassword1"] != request.form["newPassword2"]:
             flash("Your passwords must match")
-        elif hash_password(request.form["oldPassword"]) == g.user.password_hash:
-            User.query.get(g.user.id).password_hash = hash_password(request.form["newPassword1"])
+        elif hash_password(request.form["oldPassword"]) ==\
+                g.user.password_hash:
+            User.query.get(g.user.id).password_hash =\
+                hash_password(request.form["newPassword1"])
             db.session.commit()
             flash("Password succesfully changed")
         else:
